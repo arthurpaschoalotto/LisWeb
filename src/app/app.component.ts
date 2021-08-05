9432b44b41f4594cbbbc2650421c39d30db36caf
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2021-08-05 15:43:43 -0300

    Testes de cadastro paciente
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
+import { cadastropacienteComponent } from './cadastroPaciente-p1/cadastroPaciente-p1.component';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent { //tem que ser declarado em todos TS que forem usar essa informação
   title = 'Lisweb';
+  valorSalvar:any[] = [];
+
+  salvar($event: any){ //Evento para armazenar o valor após salvar
+    console.log($event);
+    const valorSalvar = {...$event}
+    this.valorSalvar.push(valorSalvar);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
-import { NgModule } from '@angular/core';
+import { cadastropacienteComponent } from './cadastroPaciente-p1/cadastroPaciente-p1.component';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastrospacientesComponent } from './cadastrospacientes/cadastrospacientes.component';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import {NgxMaskModule} from'ngx-mask';
+import { CabecalhohomeComponent } from './cabecalhohome/cabecalhohome.component';
+import { CadastroPacienteP2Component } from './cadastro-paciente-p2/cadastro-paciente-p2.component';
+import { EnderecopacienteComponent } from './cadastro-paciente-p2/enderecopaciente/enderecopaciente.component';
+
+registerLocaleData(localePt, 'pt')
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    cadastropacienteComponent,
+    CadastrospacientesComponent,
+    CabecalhohomeComponent,
+    CadastroPacienteP2Component,
+    EnderecopacienteComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxMaskModule.forRoot({
+      dropSpecialCharacters: false //Para manter a mascara
+    }),
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
