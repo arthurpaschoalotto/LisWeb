9e7b1dcf7960b5559ff7de6f49b76554b8d1df02
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2021-08-11 09:43:47 -0300

    verificaçao
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path:'',
-  pathMatch:'full',
-  redirectTo:'cadastrogermes',
-  }]
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
