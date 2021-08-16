46177b9a61392efa323ed23de56a2c98924ada2c
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2021-08-16 08:42:04 -0300

    deixando tela responsiva
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

---
+++ src/app/cadastrointerfaceamento/cadastrointerfaceamento.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrointerfaceamento',
+  templateUrl: './cadastrointerfaceamento.component.html',
+})
+export class cadastrointerfaceamento implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
