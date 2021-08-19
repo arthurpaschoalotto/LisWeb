665e0c9fcec26221f2a4e729d73674ef95a7dfae
Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>
2021-08-19 09:31:24 -0300

    Animaçoes na aba lateral, alteraçao do layout do cabecalho
    Signed-off-by: Arthur Paschoalotto <eng.arthurpaschoalotto@gmail.com>

--- src/app/menu/abalateral/abalateral.component.ts
+++ src/app/menu/abalateral/abalateral.component.ts
@@ -4,12 +4,29 @@ import { trigger, state, style, transition, animate, animation } from '@angular/
 @Component({
   selector: 'app-abalateral',
   templateUrl: './abalateral.component.html',
-  styleUrls: ['./abalateral.component.scss']
+  styleUrls: ['./abalateral.component.scss'],
+  animations: [
+    trigger('isExpanded',[
+      state('true', style({
+        'width': '40%',
+
+      })),
+      state('false', style({
+        'width': '100%',
+
+      })),
+      transition('true <=> false', animate(300)),
+
+    ])
+  ]
 })
 export class AbalateralComponent implements OnInit {
 
   isExpanded: boolean = true;
 
+  onAnimate(){
+    this.isExpanded == true? this.isExpanded = false: this.isExpanded = true;
+  }
   constructor() { }
 
   ngOnInit(): void {
